9f6f47597dde3176713f5047f8ffd634d29cfa03
Fikry Fahrezy <ffahrezi69@gmail.com>
2022-08-04 22:09:58 +0700

    fix: api response

--- pages/api/user/member/index.ts
+++ pages/api/user/member/index.ts
@@ -2,20 +2,17 @@
 // https://github.com/vercel/next.js/tree/canary/examples/with-iron-session
 import type { NextApiRequest, NextApiResponse } from "next";
 import { withIronSessionApiRoute } from "iron-session/next";
-import User from "@/model/user";
 import { sessionOptions } from "@/lib/session";
 
 export default withIronSessionApiRoute(userRoute, sessionOptions);
 
-async function userRoute(req: NextApiRequest, res: NextApiResponse<User>) {
+async function userRoute(req: NextApiRequest, res: NextApiResponse) {
   if (req.session.user) {
     // in a real world application you might read the user id from the session and then do a database request
     // to get more information on the user if needed
     res.json({
-      prop: {
-        ...req.session.user.prop,
-        isLoggedIn: true,
-      },
+      ...req.session.user.prop,
+      isLoggedIn: true,
     });
     return;
   }
